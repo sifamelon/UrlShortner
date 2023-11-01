14b26eeceafdf8c45b63a702496d7c05d3903579
sifamelon <sifensolomon7488@gmail.com>
2023-11-01 12:41:48 +0300

    config ejs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
 const Port = process.env.Port || 3000;
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 app.listen(Port, () => console.log("started server"));
